ef02dbcb94a6ff96e32792d652f513e2b109c6ff
Yvener Dutervil <72567854+yvener41@users.noreply.github.com>
2024-06-15 01:42:49 +0000

    initial commit

--- src/api/models.py
+++ src/api/models.py
@@ -3,10 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = "user_table"
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    invoices = db.relationship("Invoice", back_populates="user")
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -15,5 +17,28 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+
+class Invoice(db.Model):
+    __tablename__ = "invoice_table"
+    id = db.Column(db.Integer, primary_key=True)
+    invoice_date = db.Column(db.String(120), unique=False, nullable=False)
+    invoice_number = db.Column(db.String(120), unique=True, nullable=False)
+    invoice_amount = db.Column(db.Float, unique=False, nullable=False)
+    # ForeignKey many side : one user can have many invoices
+    user_id = db.Column(db.ForeignKey("user_table.id")) 
+    user = db.relationship("User", back_populates="invoices")
+
+    def __repr__(self):
+        return f'<Invoice {self.invoice_number}>'
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "invoice_date": self.invoice_date,
+            "invoice_number": self.invoice_date,
+            "invoice_amount": self.invoice_amount,
+            "user_id": self.user_id
             # do not serialize the password, its a security breach
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -6,17 +6,49 @@ from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 
+#JWT Imported
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+
 api = Blueprint('api', __name__)
 
 # Allow CORS requests to this API
 CORS(api)
 
+# Token Post
+# this route is for when the user already exist and need an access token
+# create a user query with a conditional to see if the user exists, or return None
+#test on postman
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/token', methods=['POST'])
+def generate_token():
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+    # receiving the request and converting the body of
+    #the request into json format
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+
+    # Query the user table to check if the user exists
+    user = User.query.filter_by(email=email, password=password).first()
+
+    if user is None:
+        response ={
+            "msg": "Email or Password does not match."
+        }
+        return jsonify(response), 401
+    
+    access_token= create_access_token(identity=user.id)
+    response ={
+        "access_token": access_token,
+        "user_id": user.id,
+        "msg": f'Welcome {user.email}! This worked!'
     }
+    return jsonify(response), 200
 
-    return jsonify(response_body), 200
+#create a route for /signup that will add the user's email and password to the database
+#POST
+#Test on Postman
+
+#create a route for invoices that will retrieve and return the users invoices in json format
+#GET
